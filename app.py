d4ed0a28ce719467eeab3aa262330813d8e44a0d
Heidi Tapani <heidi.tapani@helsinki.fi>
2025-07-27 20:49:12 +0300

    Add two .html files and modify app.py logic

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+@app.route("/added_recipes")
+def add_recipe():
+    recipe = request.form["recipe"]
+    return render_template("/added_recipes.html",recipe=recipe)
+
+
+
