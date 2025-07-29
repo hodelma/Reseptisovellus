32f97f552a93aaa37f034116083b521b6369afd7
Heidi Tapani <heidi.tapani@helsinki.fi>
2025-07-29 19:08:51 +0300

    Add edit feature and redefine names/variables

--- all_recipes.py
+++ all_recipes.py
@@ -5,6 +5,15 @@ def get_recipes():
     
     return db.query(sql)
 
+def get_recipe(recipe_id):
+    sql = "SELECT id, title, instructions FROM recipes WHERE id = ?"
+    return db.query(sql, [recipe_id])[0]
+
 def add_recipe(title, instructions):
     sql = """INSERT INTO recipes (title, instructions) VALUES (?, ?)"""
     db.execute(sql, (title, instructions))
+
+def edit_recipe(recipe_id, title, instructions):
+    sql = """UPDATE recipes SET title = ?, instructions = ? WHERE id = ?"""
+    db.execute(sql, [title, instructions, recipe_id])
+    
--- app.py
+++ app.py
@@ -20,6 +20,21 @@ def add_recipe():
         return redirect("/added_recipes")
     return render_template("add_recipe.html")
 
+@app.route("/edit_mode/<int:recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = all_recipes.get_recipe(recipe_id)
+
+    if request.method == "GET":
+        return render_template("edit_recipe.html", recipe=recipe)
+    
+    if request.method == "POST":
+        title = request.form["title"]
+        instructions = request.form["instructions"]
+        all_recipes.edit_recipe(recipe_id, title, instructions)
+        return redirect("/added_recipes")
+    return render_template("edit_recipe.html")
+
+
 @app.route("/added_recipes", methods=["GET", "POST"])
 def added_recipes():
     recipes = all_recipes.get_recipes()
