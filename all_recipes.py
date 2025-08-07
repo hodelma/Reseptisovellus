d2586fed14b6a324bf14cf48138720fddd2bcb2a
Heidi Tapani <heidi.tapani@helsinki.fi>
2025-08-08 00:11:08 +0300

    Update sql functions

--- all_recipes.py
+++ all_recipes.py
@@ -2,25 +2,41 @@ import db
 
 
 def get_recipes():
-    sql = """SELECT recipes.id, recipes.title, recipes.instructions, users.id user_id, 
-                users.username
-            FROM recipes JOIN users ON recipes.user_id = users.id
+    sql = """SELECT recipes.id,
+                    recipes.title,
+                    recipes.instructions,
+                    types.title type,
+                    diets.title diet,
+                    users.id user_id,
+                    users.username
+            FROM recipes
+            JOIN types ON recipes.type_id = types.id
+            JOIN diets ON recipes.diet_id = diets.id
+            JOIN users ON recipes.user_id = users.id
             ORDER BY recipes.id"""
     return db.query(sql)
 
 
 def get_recipe(recipe_id):
-    sql = """SELECT recipes.id, recipes.title, recipes.instructions, 
-                users.id user_id, users.username
-            FROM recipes, users WHERE recipes.user_id = users.id
-            AND recipes.id = ?"""
+    sql = """SELECT recipes.id, 
+                    recipes.title, 
+                    recipes.instructions,
+                    types.title type,
+                    diets.title diet,
+                    users.id user_id, 
+                    users.username
+            FROM recipes
+            JOIN types ON recipes.type_id = types.id
+            JOIN diets ON recipes.diet_id = diets.id
+            JOIN users ON recipes.user_id = users.id
+            WHERE recipes.id = ?"""
     result = db.query(sql, [recipe_id])
     return result[0] if result else None
 
 
-def add_recipe(title, instructions, user_id):
-    sql = """INSERT INTO recipes (title, instructions, user_id) VALUES (?, ?, ?)"""
-    db.execute(sql, (title, instructions, user_id))
+def add_recipe(title, instructions, type_id, diet_id, user_id):
+    sql = """INSERT INTO recipes (title, instructions, type_id, diet_id, user_id) VALUES (?, ?, ?, ?, ?)"""
+    db.execute(sql, (title, instructions, type_id, diet_id, user_id))
 
 
 def edit_recipe(recipe_id, title, instructions):
