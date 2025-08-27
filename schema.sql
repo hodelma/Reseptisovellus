d83c35ad0b6d3c275f9ec14cb06c0eb61cfe34c8
Heidi Tapani <heidi.tapani@helsinki.fi>
2025-08-27 16:25:38 +0300

    Create different indexes for testing performance

--- schema.sql
+++ schema.sql
@@ -37,7 +37,10 @@ CREATE TABLE connect_recipe_diets (
     diet_id INTEGER REFERENCES diets
 );
 
-CREATE INDEX idx_recipes_sent_at ON recipes(sent_at);
+CREATE INDEX idx_recipes_user_sent_at ON recipes(user_id, sent_at DESC);
+CREATE INDEX idx_recipes_type ON recipes(type_id);
 CREATE INDEX idx_comments_recipe ON comments(recipe_id);
-CREATE INDEX idx_recipe_diets_recipe ON connect_recipe_diets(recipe_id);
-CREATE INDEX idx_recipes_user ON recipes(user_id);
+CREATE INDEX idx_comments_user ON comments(user_id);
+CREATE INDEX idx_connect_recipe_diets_recipe ON connect_recipe_diets(recipe_id);
+CREATE INDEX idx_connect_recipe_diets_diet ON connect_recipe_diets(diet_id);
+CREATE INDEX idx_comments_recipe_rating ON comments(recipe_id, rating);
